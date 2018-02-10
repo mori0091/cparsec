78646ea732fed2ceaf8d5c514c78154a2170834d
mori0091 <mori-d@qc4.so-net.ne.jp>
2018-02-10 15:27:19 +0900

    added: primitive binary operator 'seq' and 'either'

---
+++ src/cparsec/primitive/either.c
@@ -0,0 +1,14 @@
+/* -*- mode: c++ ; coding: utf-8-unix -*- */
+
+#include "cparsec/primitive/either.h"
+
+// \todo error message should be "expected <X> but was <y>"
+DEF_GEN_PARSER__BINARY_OP( either )
+{
+  Source bak = *psrc;
+  Val x = RUN_PARSER( self->arg1.parser );
+  if ( x.type != ERROR ) return x;
+  if ( !Source_eq( &bak, psrc ) ) return x;
+  Val_del( &x );
+  return RUN_PARSER( self->arg2.parser );
+}
---
+++ src/cparsec/primitive/seq.c
@@ -0,0 +1,13 @@
+/* -*- mode: c++ ; coding: utf-8-unix -*- */
+
+#include "cparsec/primitive/seq.h"
+
+// \todo error message should be "expected <X> but was <y>"
+DEF_GEN_PARSER__BINARY_OP( seq )
+{
+  Val x = RUN_PARSER( self->arg1.parser );
+  if ( x.type == ERROR ) return x;
+  Val y = RUN_PARSER( self->arg2.parser );
+  if ( y.type == ERROR ) { Val_del( &x ); return y; }
+  return Val_concat( &x, &y );
+}
