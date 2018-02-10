52e544c0f12e5d97b82ce4a4d1ee25b0b9aac861
mori0091 <mori-d@qc4.so-net.ne.jp>
2018-02-10 19:47:04 +0900

    added: primitive unary operator 'many'

---
+++ include/cparsec/primitive/many.h
@@ -0,0 +1,24 @@
+/* -*- mode: c++ ; coding: utf-8-unix -*- */
+
+#pragma once
+
+#ifndef CPARSEC_PRIMITIVE_MANY_H
+#define CPARSEC_PRIMITIVE_MANY_H
+
+#include "cparsec/gen_parser/unary_op.h"
+
+#define MANY( x, y )            UNARY_OP( many, (x) )
+#define MANY_ST( x, y )         UNARY_OP_ST( many, (x) )
+#define MANY_ST_INIT( x, y )    UNARY_OP_ST_INIT( many, (x) )
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  extern DECL_PARSER_FUNC( many );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* CPARSEC_PRIMITIVE_MANY_H */
---
+++ test/unit_many.cpp
@@ -0,0 +1,123 @@
+/* -*- mode: c++ ; coding: utf-8-unix -*- */
+
+#include <string>
+#include "catch.hpp"
+
+#include "cparsec.h"
+
+SCENARIO( "'many' parser combinator", "[cparsec][many]" ) {
+  GIVEN( "Parser a = char1('a')" ) {
+    Parser a = char1('a');
+    WHEN( "parse( many(a), \"\")" ) {
+      const char* input  = "";
+      std::string expect = "";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"a\")" ) {
+      const char* input  = "a";
+      std::string expect = "a";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"b\")" ) {
+      const char* input  = "b";
+      std::string expect = "";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"c\")" ) {
+      const char* input  = "c";
+      std::string expect = "";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"ab\")" ) {
+      const char* input  = "ab";
+      std::string expect = "a";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"abc\")" ) {
+      const char* input  = "abc";
+      std::string expect = "a";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"ac\")" ) {
+      const char* input  = "ac";
+      std::string expect = "a";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"ba\")" ) {
+      const char* input  = "ba";
+      std::string expect = "";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"bab\")" ) {
+      const char* input  = "bab";
+      std::string expect = "";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"aaa\")" ) {
+      const char* input  = "aaa";
+      std::string expect = "aaa";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    WHEN( "parse( many(a), \"aaabbb\")" ) {
+      const char* input  = "aaabbb";
+      std::string expect = "aaa";
+      Val ret = parse( many(a), input );
+      THEN( "results a string \"" + expect + "\"" ) {
+        REQUIRE( ret.type == STRING );
+        REQUIRE( expect == ret.str );
+      }
+      Val_del( &ret );
+    }
+    Parser_del( a );
+  }
+}
