aaa1f6aa0fa63437b6fccd5d11e10f0dd580922b
mori0091 <mori-d@qc4.so-net.ne.jp>
2018-02-12 09:17:00 +0900

    added: primitive unary operator 'tryp'

---
+++ src/cparsec/primitive/tryp.c
@@ -0,0 +1,14 @@
+/* -*- mode: c++ ; coding: utf-8-unix -*- */
+
+#include "cparsec/primitive/tryp.h"
+
+// \todo error message should be "expected <X> but was <y>"
+DEF_GEN_PARSER__UNARY_OP( tryp )
+{
+    Source bak = *psrc;
+    Val ret = RUN_PARSER( self->arg1.parser );
+    if ( ret.type == ERROR ) {
+        *psrc = bak;
+    }
+    return ret;
+}
