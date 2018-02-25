d320f0bacf3b821a4fdd6c4176c4b974d8845a00
mori0091 <mori-d@qc4.so-net.ne.jp>
2018-02-25 11:03:49 +0900

    added: An exmaple code for Fn_apply() - [currying][partial application]

---
+++ example/Project.mk
@@ -0,0 +1,8 @@
+# -*- mode: makefile ; coding: utf-8-unix -*-
+
+# TARGET = $(BIN_DIR)/example
+
+REQUIRE = ../../cparsec
+
+CFLAGS   += --std=c11 -D_GNU_SOURCE
+CXXFLAGS += --std=c++11 -D_GNU_SOURCE
